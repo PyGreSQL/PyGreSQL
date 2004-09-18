858e95ec88593645f9a7a8b0b154f7d952617e71
D'Arcy J.M. Cain <darcy@druid.net>
2004-09-18 11:34:56 +0000

    Wrapper for DB-API 2.0 test.
    Test can be picked up from http://zen.freezope.org/Software/DBAPI20TestSuite/

---
+++ module/TEST_PyGreSQL_dbapi20.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import dbapi20
+import unittest
+import pgdb
+import popen2
+
+class test_Psycopg(dbapi20.DatabaseAPI20Test):
+    driver = pgdb
+    connect_args = ()
+    connect_kw_args = {'dsn': ':dbapi20_test'}
+
+    lower_func = 'lower' # For stored procedure test
+
+    def setUp(self):
+        # Call superclass setUp In case this does something in the
+        # future
+        dbapi20.DatabaseAPI20Test.setUp(self) 
+
+        con = self._connect()
+        con.close()
+        try:
+            con = self._connect()
+            con.close()
+        except:
+            cmd = "psql -c 'create database dbapi20_test'"
+            cout,cin = popen2.popen2(cmd)
+            cin.close()
+            cout.read()
+
+    def tearDown(self):
+        dbapi20.DatabaseAPI20Test.tearDown(self)
+
+    def test_nextset(self): pass
+    def test_setoutputsize(self): pass
+
+if __name__ == '__main__':
+    unittest.main()
