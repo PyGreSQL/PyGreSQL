f2c620bf9b05568ca51ebf8b436b0dc164a5387c
D'Arcy J.M. Cain <darcy@druid.net>
2006-01-22 12:47:38 +0000

    Add unit tests for get().

--- module/TEST_PyGreSQL_classic.py
+++ module/TEST_PyGreSQL_classic.py
@@ -22,10 +22,15 @@ class utility_test(unittest.TestCase):
         for t in ('_test1', '_test2'):
             try: db.query("CREATE SCHEMA " + t)
             except: pass
-            try: db.query("CREATE TABLE %s._test_schema (%s int PRIMARY KEY)" % (t, t))
+            try: db.query("CREATE TABLE %s._test_schema "
+							"(%s int PRIMARY KEY)" % (t, t))
             except: pass
 
         try: db.query("CREATE TABLE _test_schema (_test int PRIMARY KEY)")
+        except: pass
+
+        try: db.query("CREATE VIEW _test_vschema AS "
+			"SELECT _test, 'abc'::text AS _test2  FROM _test_schema")
         except: pass
 
     def test_invalidname(self):
@@ -61,6 +66,15 @@ class utility_test(unittest.TestCase):
         self.assertEqual(db.pkey('_test_schema'), '_test')
         self.assertEqual(db.pkey('public._test_schema'), '_test')
         self.assertEqual(db.pkey('_test1._test_schema'), '_test1')
+
+    def test_get(self):
+        try: db.query("INSERT INTO _test_schema VALUES (1234)")
+        except: pass # OK if it already exists
+
+        db.get('_test_schema', 1234)
+        db.get('_test_schema', 1234, keyname = '_test')
+        self.failUnlessRaises(KeyError, db.get, '_test_vschema', 1234)
+        db.get('_test_vschema', 1234, keyname = '_test')
 
 if __name__ == '__main__':
     unittest.main()
