81ab30a7977fd29939108635916b2bfabc0e7be8
D'Arcy J.M. Cain <darcy@druid.net>
2006-06-01 23:14:12 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_3_8_1'.

---
+++ RELEASE_3_8_1/module/TEST_PyGreSQL_classic.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+
+import sys, unittest
+from pg import *
+
+# We need a database to test against.  If LOCAL_PyGreSQL.py exists we will
+# get our information from that.  Otherwise we use the defaults.
+dbname = 'unittest'
+dbhost = None
+dbport = 5432
+
+try: from LOCAL_PyGreSQL import *
+except: pass
+
+db = DB(dbname, dbhost, dbport)
+db.query("SET DATESTYLE TO 'ISO'")
+db.query("SET TIME ZONE 'EST5EDT'")
+db.query("SET DEFAULT_WITH_OIDS=TRUE")
+
+class utility_test(unittest.TestCase):
+
+    def setUp(self):
+        # create test tables if they don't exist
+        for t in ('_test1', '_test2'):
+            try: db.query("CREATE SCHEMA " + t)
+            except: pass
+            try: db.query("CREATE TABLE %s._test_schema "
+                "(%s int PRIMARY KEY)" % (t, t))
+            except: pass
+
+        try: db.query("CREATE TABLE _test_schema "
+                       "(_test int PRIMARY KEY, _i interval)")
+        except: pass
+
+        try: db.query("CREATE VIEW _test_vschema AS "
+            "SELECT _test, 'abc'::text AS _test2  FROM _test_schema")
+        except: pass
+
+    def test_invalidname(self):
+        """Make sure that invalid table names are caught"""
+
+        self.failUnlessRaises(ProgrammingError, db.get_attnames, 'x.y.z')
+
+    def test_schema(self):
+        """Does it differentiate the same table name in different schemas"""
+
+        # see if they differentiate the table names properly
+        self.assertEqual(
+            db.get_attnames('_test_schema'),
+            {'_test': 'int', 'oid': 'int', '_i': 'date'}
+        )
+
+        self.assertEqual(
+            db.get_attnames('public._test_schema'),
+            {'_test': 'int', 'oid': 'int', '_i': 'date'}
+        )
+
+        self.assertEqual(
+            db.get_attnames('_test1._test_schema'),
+            {'_test1': 'int', 'oid': 'int'}
+        )
+
+        self.assertEqual(
+            db.get_attnames('_test2._test_schema'),
+            {'_test2': 'int', 'oid': 'int'}
+        )
+
+    def test_pkey(self):
+        self.assertEqual(db.pkey('_test_schema'), '_test')
+        self.assertEqual(db.pkey('public._test_schema'), '_test')
+        self.assertEqual(db.pkey('_test1._test_schema'), '_test1')
+
+        self.assertEqual(db.pkey('_test_schema',
+                {'test1': 'a', 'test2.test3': 'b'}),
+                {'public.test1': 'a', 'test2.test3': 'b'})
+        self.assertEqual(db.pkey('test1'), 'a')
+        self.assertEqual(db.pkey('public.test1'), 'a')
+
+    def test_get(self):
+        try: db.query("INSERT INTO _test_schema VALUES (1234)")
+        except: pass # OK if it already exists
+
+        db.get('_test_schema', 1234)
+        db.get('_test_schema', 1234, keyname = '_test')
+        self.failUnlessRaises(KeyError, db.get, '_test_vschema', 1234)
+        db.get('_test_vschema', 1234, keyname = '_test')
+
+    def test_quote(self):
+        from pg import _quote
+        self.assertEqual(_quote(1, 'int'), "1")
+        self.assertEqual(_quote(1, 'text'), "'1'")
+        self.assertEqual(_quote(1, 'seq'), "1")
+        self.assertEqual(_quote(1, 'decimal'), "1")
+        self.assertEqual(_quote('1', 'int'), "1")
+        self.assertEqual(_quote('1', 'text'), "'1'")
+        self.assertEqual(_quote('1', 'seq'), "1")
+        self.assertEqual(_quote('1', 'decimal'), "1")
+        self.assertEqual(_quote(None, 'int'), "NULL")
+        self.assertEqual(_quote(1, 'money'), "'1.00'")
+        self.assertEqual(_quote('1', 'money'), "'1.00'")
+        self.assertEqual(_quote(1.234, 'money'), "'1.23'")
+        self.assertEqual(_quote('1.234', 'money'), "'1.23'")
+        self.assertEqual(_quote(0, 'bool'), "'f'")
+        self.assertEqual(_quote('', 'bool'), "NULL")
+        self.assertEqual(_quote('f', 'bool'), "'f'")
+        self.assertEqual(_quote('off', 'bool'), "'f'")
+        self.assertEqual(_quote('no', 'bool'), "'f'")
+        self.assertEqual(_quote(1, 'bool'), "'t'")
+        self.assertEqual(_quote('1', 'bool'), "'t'")
+        self.assertEqual(_quote('t', 'bool'), "'t'")
+        self.assertEqual(_quote('on', 'bool'), "'t'")
+        self.assertEqual(_quote('yes', 'bool'), "'t'")
+        self.assertEqual(_quote('true', 'bool'), "'t'")
+        self.assertEqual(_quote('y', 'bool'), "'t'")
+        self.assertEqual(_quote('', 'date'), "NULL")
+        self.assertEqual(_quote('', 'inet'), "NULL")
+        self.assertEqual(_quote('', 'cidr'), "NULL")
+        self.assertEqual(_quote('date', 'date'), "'date'")
+        self.assertEqual(_quote('inet', 'inet'), "'inet'")
+        self.assertEqual(_quote('cidr', 'cidr'), "'cidr'")
+        self.assertEqual(_quote('', 'text'), "''")
+        self.assertEqual(_quote("\\", 'text'), "'\\\\'")
+        self.assertEqual(_quote("'", 'text'), "''''")
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ RELEASE_3_8_1/module/TEST_PyGreSQL_dbapi20.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# $Id: TEST_PyGreSQL_dbapi20.py,v 1.4 2006-05-28 19:18:50 cito Exp $
+
+import dbapi20
+import unittest
+import pgdb
+import popen2
+
+# We need a database to test against.  If LOCAL_PyGreSQL.py exists we will
+# get our information from that.  Otherwise we use the defaults.
+dbname = 'dbapi20_test'
+dbhost = None
+dbport = 5432
+
+class test_Psycopg(dbapi20.DatabaseAPI20Test):
+    driver = pgdb
+    connect_args = ()
+    connect_kw_args = {'dsn': ':' + dbname}
+
+    lower_func = 'lower' # For stored procedure test
+
+    def setUp(self):
+        # Call superclass setUp In case this does something in the
+        # future
+        dbapi20.DatabaseAPI20Test.setUp(self)
+
+        con = self._connect()
+        con.close()
+        try:
+            con = self._connect()
+            con.close()
+        except:
+            cmd = "psql -c 'create database dbapi20_test'"
+            cout,cin = popen2.popen2(cmd)
+            cin.close()
+            cout.read()
+
+    def tearDown(self):
+        dbapi20.DatabaseAPI20Test.tearDown(self)
+
+    def test_nextset(self): pass
+    def test_setoutputsize(self): pass
+
+if __name__ == '__main__':
+    unittest.main()
