5e7bf13023fdb03a23a53a6edf88e3d00c6741dd
Christoph Zwerschke <cito@online.de>
2016-02-06 21:47:07 +0000

    Avoid duplication in the title of the docs
    The actual documentation comes only at the second level in the breadcrumb.

--- docs/conf.py
+++ docs/conf.py
@@ -142,7 +142,7 @@ html_theme_path = ['_themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = None if use_cloud_theme else 'PyGreSQL %s' % version
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
