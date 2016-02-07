f36825c2282e6cde7746c337de7c7cd9f0cdfa86
Christoph Zwerschke <cito@online.de>
2016-02-07 12:29:50 +0000

    Minor fixes in the docs configuration

--- docs/conf.py
+++ docs/conf.py
@@ -133,7 +133,13 @@ html_theme = 'cloud' if use_cloud_theme else 'default'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {'defaultcollapsed': True} if use_cloud_theme else {}
+if use_cloud_theme:
+    html_theme_options = {
+        'roottarget': 'contents/index',
+        'defaultcollapsed': True,
+        'shaded_decor': True}
+else:
+    html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ['_themes']
