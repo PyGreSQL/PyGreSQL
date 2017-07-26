5c3223d2e09cc02c4d95cc6eb2365897c6d034c0
Christoph Zwerschke <cito@online.de>
2017-07-26 16:38:26 +0000

    Bump version number
    Also add Python 3.6 classifier for the next release.
    5.0.4 actually already supports Python 3.6.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ using distutils to install Python programs.
 
 """
 
-version = '5.0.4'
+version = '5.0.5'
 
 
 import sys
@@ -203,6 +203,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         "Programming Language :: SQL",
         "Topic :: Database",
         "Topic :: Database :: Front-Ends",
