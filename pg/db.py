17c42afbf46196510a7eacb822acad16de82948a
Christoph Zwerschke <cito@online.de>
2023-09-06 14:13:44 +0200

    Use different docstrings for overloaded methods

--- pg/db.py
+++ pg/db.py
@@ -42,10 +42,12 @@ class DB:
                  opt: str | None = None,
                  user: str | None = None, passwd: str | None = None,
                  nowait: bool = False) -> None:
+        """Create a new connection using the specified parameters."""
         ...
 
     @overload
     def __init__(self, db: Connection | DB | DbApi2Connection) -> None:
+        """Create a connection wrapper based on an existing connection."""
         ...
 
     def __init__(self, *args: Any, **kw: Any) -> None: 
